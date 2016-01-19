6c191088aa5e5f71d9d45f5bbfcf77069a3a0110
violetyk <yuhei.kagaya@gmail.com>
2016-01-19 13:54:40 +0900

    use @mention, error handling

--- lib/lita/handlers/git_branch_switcher.rb
+++ lib/lita/handlers/git_branch_switcher.rb
@@ -21,21 +21,37 @@ module Lita
       end
 
       route(/^current$/, command: true) do |response|
-        response.reply(current_branch)
+        response.reply("@%{user} %{branch}" % {
+          user: response.user.mention_name,
+          branch: current_branch,
+        })
       end
 
       route(/^switch\s+(.+)/, command: true) do |response|
-        branch_name = response.matches[0][0]
+        begin
+          branch_name = response.matches[0][0]
 
-        g = Git.open(config.repository_path)
-        g.fetch
-        if g.is_branch?(branch_name)
-          g.checkout(branch_name)
-          g.pull(config.remote, branch_name) if config.pull_after_switch == true
-          systemu(config.command_after_switch) unless config.command_after_switch.nil?
-          response.reply(current_branch)
-        else
-          response.reply('[%{branch}] is not found.' % {branch: branch_name } )
+          g = Git.open(config.repository_path)
+          g.fetch
+          if g.is_branch?(branch_name)
+            g.checkout(branch_name)
+            g.pull(config.remote, branch_name) if config.pull_after_switch == true
+            systemu(config.command_after_switch) unless config.command_after_switch.nil?
+            response.reply("@%{user} %{branch}" % {
+              user: response.user.mention_name,
+              branch: current_branch,
+            })
+          else
+            response.reply("@%{user} [%{branch}] is not found." % {
+              user: response.user.mention_name,
+              branch: branch_name
+            })
+          end
+        rescue => e
+          response.reply("@%{user} I was not able to do it. \n```\n%{message}\n```" % {
+            user: response.user.mention_name,
+            message: e.message,
+          })
         end
       end
 
